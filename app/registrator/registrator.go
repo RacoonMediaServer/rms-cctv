8fbe5da5effe27c878cb10849f24f9cfae6b7bc4
Yury Zaytsev <mrenotenot@gmail.com>
2024-01-03 16:35:20 +0300

    registrator: сделана утилита для добавления камер

---
+++ app/registrator/registrator.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	rms_cctv "github.com/RacoonMediaServer/rms-packages/pkg/service/rms-cctv"
+	"github.com/urfave/cli/v2"
+	"go-micro.dev/v4"
+)
+
+func main() {
+	camera := rms_cctv.Camera{Schedule: "{}"}
+	service := micro.NewService(
+		micro.Name("rms-cctv.client"),
+		micro.Flags(
+			&cli.StringFlag{
+				Name:        "name",
+				Usage:       "Camera name",
+				Required:    true,
+				Destination: &camera.Name,
+			},
+			&cli.StringFlag{
+				Name:        "url",
+				Usage:       "Camera URL",
+				Required:    true,
+				Destination: &camera.Url,
+			},
+			&cli.StringFlag{
+				Name:        "user",
+				Usage:       "Username",
+				Required:    false,
+				Destination: &camera.User,
+			},
+			&cli.StringFlag{
+				Name:        "password",
+				Usage:       "Password",
+				Required:    false,
+				Destination: &camera.Password,
+			},
+		),
+	)
+	service.Init()
+
+	client := rms_cctv.NewRmsCctvService("rms-cctv", service.Client())
+	_, err := client.AddCamera(context.Background(), &camera)
+	if err != nil {
+		panic(err)
+	}
+}
